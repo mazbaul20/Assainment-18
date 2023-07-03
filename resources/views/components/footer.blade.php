3164962cfb46fef1a39b351fa699c7bfd2697138
MD Mazbaul Islam <mazbaul20@gmail.com>
2023-07-03 20:22:48 +0600

    Assignment module-18

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+
+<footer class="py-4 shadow-sm text-white bg-dark mt-auto">
+    <div class="container px-5">
+        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+            <div class="col-auto"><div class="small m-0">Copyright &copy; Mazbaul islam 2023</div></div>
+        </div>
+    </div>
+</footer>
